fa36d7810aa709eed3518969cfcf943a322e9573
l3miage-ifeghm <manal.ifegh@etu.univ-grenoble-alpes.fr>
2024-03-13 22:47:09 +0100

    problème test bizzare

--- server/src/main/java/fr/uga/l3miage/tp1/exo1/models/ClassTestJPATwo.java
+++ server/src/main/java/fr/uga/l3miage/tp1/exo1/models/ClassTestJPATwo.java
@@ -1,2 +1,19 @@
-package fr.uga.l3miage.tp1.exo1.models;public class ClassTestJPATwo {
+package fr.uga.l3miage.tp1.exo1.models;
+
+
+import javax.persistence.*;
+import java.util.Date;
+@Entity
+public class ClassTestJPATwo {
+    @Id
+    private String myIdTest;
+
+    @Column(unique=true)
+    private String testName;
+
+    @Column(precision = 1)
+    private Double jpaSeed;
+
+    @Column()
+    private Date savedAt;
 }
